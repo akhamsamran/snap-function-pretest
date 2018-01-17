9d212531122d71c93a856f3da80546a6d2f62516
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-17 08:37:09 -0700

    added questions

---
+++ function.js
@@ -0,0 +1,18 @@
+/*repository: snap-function-pretest
+write a function that (one per file)
+0) uses an if block to return true if the input is the answer to life, the universe and everything - false if not
+	1) capitalizes the first letter of a string that is input
+2) sums an array WITHOUT the use of system functions such as array_sum()
+3) rewrite str_repeat(): repeat the string (first input) the number of times specified (second input)
+4) find the dot product of two given arrays
+*/
+let answerToLife = true;
+if (answerToLife===true) {
+	console.log('0- Yes, this is the answer to life, the universe and everything')
+} else {
+	console.log('0- No, keep looking for the answer')
+};
+
+let myString= 'Wafer marshmallow tootsie roll croissant jelly beans. Tootsie roll macaroon gingerbread lemon drops tootsie roll chocolate cake marshmallow dragée. Donut chocolate cake chocolate lollipop.'.toLowerCase();
+console.log('1- '+myString);
+//sum arrray without sum array function:
